97a8153ed3d88dd6127297468b30858836b6a272
Akshayshah47 <removeit61@gmail.com>
2023-06-15 20:42:05 +0530

    optimised the code->readFile

--- models/product.js
+++ models/product.js
@@ -3,35 +3,36 @@ const path = require('path');
 
 const rootDir = require('../util/path');
 
+const p=path.join(rootDir,'data','product.json');
+
+const getProductsArray=(cb)=>{
+    fs.readFile(p,(err,data)=>{
+        if(!err){
+            cb(JSON.parse(data));
+        }else{
+            cb([]);
+        }
+    });
+}
+
 module.exports = class Procuct {
     constructor(title) {
         this.title = title;
     }
     save() {
-        let products = [];
-        fs.readFile(path.join(rootDir, 'data', 'product.json'), (err, datacontent) => {
-            if (!err) {
-                products = JSON.parse(datacontent);
-            }
+        getProductsArray(products=>{
             products.push(this);
-            fs.writeFile(path.join(rootDir, 'data', 'product.json'), JSON.stringify(products), err => {
+            fs.writeFile(p, JSON.stringify(products), err => {
                 if (err) {
                     console.log(err.message);
                 } else {
                     console.log("inserted");
                 }
             });
-
-        });
+        })
 
     }
     static fetchAll(cb) {
-        fs.readFile(path.join(rootDir,'data','product.json'),(err,data)=>{
-            if(!err){
-                cb(JSON.parse(data));
-            }else{
-                cb([]);
-            }
-        });
+        getProductsArray(cb);
     }
 }
